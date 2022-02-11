75d6ead1fb029424d7af4eeb0d29b1f4802dd125
Gabriel Sessions <gsess7732@gmail.com>
2022-02-10 19:24:45 -0500

    json files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'homepage': 'url("./bg_img.jpg")',
+        'green-line': 'url("./mbta_green.png")'
+      },
+      
+    },
+  },
+  plugins: [],
+}
